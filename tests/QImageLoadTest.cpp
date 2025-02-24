c38ebf9e46deb501af79f7f5e3cc1930c5b8065f
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2025-02-24 13:51:00 +0800

    test: add QImageLoadTest

--- tests/QImageLoadTest.cpp
+++ tests/QImageLoadTest.cpp
@@ -2,16 +2,51 @@
 // Created by Lyndon on 2025/1/26.
 //
 
+#include "qgsgltf3dutils.h"
+
+#include "qgsgltfutils.h"
+#include "qgsblockingnetworkrequest.h"
+#include "qgscoordinatetransform.h"
+#include "qgslogger.h"
+#include "qgsmetalroughmaterial.h"
+#include "qgstexturematerial.h"
+
+#include <Qt3DCore/QEntity>
+
+#if QT_VERSION < QT_VERSION_CHECK( 6, 0, 0 )
+#include <Qt3DRender/QAttribute>
+#include <Qt3DRender/QBuffer>
+#include <Qt3DRender/QGeometry>
+typedef Qt3DRender::QAttribute Qt3DQAttribute;
+typedef Qt3DRender::QBuffer Qt3DQBuffer;
+typedef Qt3DRender::QGeometry Qt3DQGeometry;
+#else
+#include <Qt3DCore/QAttribute>
+#include <Qt3DCore/QBuffer>
+#include <Qt3DCore/QGeometry>
+typedef Qt3DCore::QAttribute Qt3DQAttribute;
+typedef Qt3DCore::QBuffer Qt3DQBuffer;
+typedef Qt3DCore::QGeometry Qt3DQGeometry;
+#endif
+
+#include <Qt3DRender/QGeometryRenderer>
+#include <Qt3DRender/QTexture>
+
+#include <QFile>
+#include <QFileInfo>
+#include <QMatrix4x4>
+#include "qgsmatrix4x4.h"
+
+
 #include "gtest/gtest.h"
 #include "utils/Formula.h"
 #include "qgis_3d.h"
-#include "qgsgltf3dutils.h"
 #include <QImage>
 #include <QUrl>
 #include <QNetworkRequest>
 #include <QNetworkReply>
-#include <QFile>
-#include "qgsblockingnetworkrequest.h"
+#define TINYGLTF_NO_STB_IMAGE       // we use QImage-based reading of images
+#define TINYGLTF_NO_STB_IMAGE_WRITE // we don't need writing of images
 #include "tiny_gltf.h"
 
 class QImageLoadTest : public ::testing::Test {
