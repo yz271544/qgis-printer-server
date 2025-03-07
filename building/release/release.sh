4b65dc0bf2c4c58df30b9c118b36ddf4175083eb
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2025-03-07 10:51:35 +0800

    add release.sh

--- building/release/release.sh
+++ building/release/release.sh
@@ -1,14 +1,52 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+
+if [ -z "$1" ]; then
+    echo "第一个参数不能为空，请输入release发布描述。"
+    exit 1
+else
+    echo "发布描述：$1"
+fi
 
 current_tag=$(git describe --tags)
 
+if [[ $current_tag =~ -g ]]; then
+    echo "tag有偏差"
+    exit 1
+fi
+
+if [ -z "$current_tag" ]; then
+  echo "No tag found"
+  exit 1
+fi
+
+if [ $(git rev-parse --abbrev-ref HEAD) != "master" ]; then
+  echo "Not on master branch"
+  exit 1
+fi
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Working directory not clean"
+  exit 1
+fi
+
+if [ -n "$(git log origin/master..HEAD)" ]; then
+  echo "Not all changes pushed"
+  exit 1
+fi
+
+if [ -n "$(git tag --points-at HEAD)" ]; then
+  echo "Tag already exists"
+  exit 1
+fi
+
+if
 
 curl --request POST --header "PRIVATE-TOKEN: mhVqLTzGk_V2yMBeiwwf" \
---data name="release-v1.0.17" \
---data tag_name="v1.0.17" \
+--data name="release-${current_tag}" \
+--data tag_name="${current_tag}" \
 --data url="http://172.31.100.21/zhengyang.hu/jingweiprinter" \
---data ref="v1.0.17" \
---data description="接口异步执行，主线程渲染后回调响应" \
+--data ref="${current_tag}" \
+--data description="$1" \
 http://172.31.100.21/api/v4/projects/64/releases
 
 
