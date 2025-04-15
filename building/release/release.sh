359490be9863047342233c66d37f96bcdad2d880
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2025-04-15 11:50:59 +0800

    modify the gitignore

--- building/release/release.sh
+++ building/release/release.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
 if [ -z "$1" ]; then
-    echo "第一个参数不能为空，请输入release发布描述。"
-    exit 1
+  echo "第一个参数不能为空，请输入release发布描述。"
+  exit 1
 else
-    echo "发布描述：$1"
+  echo "发布描述：$1"
 fi
 
 current_tag=$(git describe --tags)
 
 if [[ $current_tag =~ -g ]]; then
-    echo "tag有偏差"
-    exit 1
+  echo "tag有偏差"
+  exit 1
 fi
 
 if [ -z "$current_tag" ]; then
@@ -39,16 +39,13 @@ if [ -n "$(git tag --points-at HEAD)" ]; then
   exit 1
 fi
 
-if
-
+echo "create release from tag ${current_tag}"
 curl --request POST --header "PRIVATE-TOKEN: mhVqLTzGk_V2yMBeiwwf" \
---data name="release-${current_tag}" \
---data tag_name="${current_tag}" \
---data url="http://172.31.100.21/zhengyang.hu/jingweiprinter" \
---data ref="${current_tag}" \
---data description="$1" \
-http://172.31.100.21/api/v4/projects/64/releases
-
+  --data name="release-${current_tag}" \
+  --data tag_name="${current_tag}" \
+  --data url="http://172.31.100.21/zhengyang.hu/jingweiprinter" \
+  --data ref="${current_tag}" \
+  --data description="$1" \
+  http://172.31.100.21/api/v4/projects/64/releases
 
 curl --header "PRIVATE-TOKEN: mhVqLTzGk_V2yMBeiwwf" http://172.31.100.21/api/v4/projects/64/releases
-
